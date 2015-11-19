29c74764a9fa7f475243d93eef79d4047a96c16f
Roger Sperberg <rsperberg@gmail.com>
2015-11-18 22:18:23 -0500

    initial commit

---
+++ WeatherProject/index.android.js
@@ -0,0 +1,12 @@
+/**
+ * WeatherProject React Native App from Bonnie Eisenman, Learning React Native (O'Reilly), p45
+ * https://github.com/facebook/react-native
+ */
+'use strict';
+
+var React = require('react-native');
+var {
+  AppRegistry
+} = React;
+
+var WeatherProject = require('./WeatherProject'); AppRegistry.registerComponent('WeatherProject', () => WeatherProject);
