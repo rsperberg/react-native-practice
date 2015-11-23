6ea46981bc0c5d577066204f3dd08f12232658cb
Roger Sperberg <rsperberg@gmail.com>
2015-11-22 20:33:36 -0500

    use version from bonniee repo

--- WeatherProject/WeatherProject.js
+++ WeatherProject/WeatherProject.js
@@ -1,7 +1,3 @@
-/**
- * WeatherProject React Native App from Bonnie Eisenman, Learning React Native (O'Reilly), p45
- * https://github.com/facebook/react-native
- */
 var React = require('react-native');
 var {
   StyleSheet,
@@ -14,67 +10,61 @@ var {
 var Forecast = require('./Forecast');
 
 var WeatherProject = React.createClass({
-  // Could enter default zip code here
-  getInitialState() {
-  	return ({
-  	  zip: '',
-  	  forecast: null
-  	});
-  },
-  // We pass this callback to the <TextInput>
-  _handleTextChange(event) {
-   	var zip = event.nativeEvent.text;
- 	// log statements are viewable in Xcode
-  	// and in the Chrome debug tools
-  	console.log(zip);
-  	this.setState({zip: zip});
-  	fetch('http://api.openweathermap.org/data/2.5/weather?q=' + zip + '&units=imperial')
-  		.then((response) => response.json())
-  		.then((responseJSON) => {
-  		// just taking a look at the format
-  		console.log(responseJSON);
-  		this.setState({
-  			forecast: {
-  				main: responseJSON.weather[0].main,
-  				description: responseJSON.weather[0].description,
-  				temp: responseJSON.main.temp
-  			}
-  		});
-  	})
-  	.catch((error) => {
-  		console.warn(error);
-  	});
+  getInitialState: function() {
+    return {
+      zip: '',
+      forecast: null
+    };
   },
 
-  render() {
-  	var content = null;
-  	if (this.state.forecast !== null) {
-  		content = <Forecast
-  					main={this.state.forecast.main}
-  					description={this.state.forecast.description}
-  					temp={this.state.forecast.temp} />;
-  	}
+  _handleTextChange: function(event) {
+    var zip = event.nativeEvent.text;
+    this.setState({zip: zip});
+    fetch('http://api.openweathermap.org/data/2.5/weather?q='
+      + zip + '&units=imperial')
+      .then((response) => response.json())
+      .then((responseJSON) => {
+        this.setState({
+          forecast: {
+            main: responseJSON.weather[0].main,
+            description: responseJSON.weather[0].description,
+            temp: responseJSON.main.temp
+          }
+        });
+      })
+      .catch((error) => {
+        console.warn(error);
+      });
+  },
+
+  render: function() {
+    var content = null;
+    if (this.state.forecast !== null) {
+      content = <Forecast
+                  main={this.state.forecast.main}
+                  description={this.state.forecast.description}
+                  temp={this.state.forecast.temp}/>;
+    }
     return (
-    <View style={styles.container}>
-		<Image source={require('image!flowers')}
-			resizeMode='cover'
-			style={styles.backdrop}>
-			<View style={styles.overlay}>
-				<View style={styles.row}>
-				    <Text style={styles.mainText}>
-				    	Current weather for
-				    </Text>
-				    <View style={styles.zipContainer}>
-				    	<TextInput
-					    	style={[styles.zipCode, styles.mainText]}
-					    	returnKeyType='go'
-					     	onSubmitEditing={this._handleTextChange} />
-					</View>
-				</View>
-			    {content}
-			</View>
-		</Image>
-	</View>
+      <View style={styles.container}>
+        <Image source={require('image!flowers')}
+               resizeMode='cover'
+               style={styles.backdrop}>
+          <View style={styles.overlay}>
+           <View style={styles.row}>
+             <Text style={styles.mainText}>
+               Current weather for
+             </Text>
+             <View style={styles.zipContainer}>
+               <TextInput
+                 style={[styles.zipCode, styles.mainText]}
+                 onSubmitEditing={this._handleTextChange}/>
+             </View>
+           </View>
+           {content}
+         </View>
+        </Image>
+      </View>
     );
   }
 });
@@ -85,39 +75,39 @@ var styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: 'center',
-	paddingTop: 30
+    paddingTop: 30
   },
   backdrop: {
-  	flex: 1,
-  	flexDirection: 'column'
+    flex: 1,
+    flexDirection: 'column'
   },
   overlay: {
-  	paddingTop: 5,
-  	backgroundColor: '#000000',
-  	opacity: 0.5,
-  	flexDirection: 'column',
-  	alignItems: 'center'
+    paddingTop: 5,
+    backgroundColor: '#000000',
+    opacity: 0.5,
+    flexDirection: 'column',
+    alignItems: 'center'
   },
   row: {
-  	flex: 1,
-  	flexDirection: 'row',
-  	flexWrap: 'nowrap',
-  	alignItems: 'flex-start',
-  	padding: 30
+    flex: 1,
+    flexDirection: 'row',
+    flexWrap: 'nowrap',
+    alignItems: 'flex-start',
+    padding: 30
   },
   zipContainer: {
-  	flex: 1,
-  	borderBottomColor: '#DDDDDD',
-  	borderBottomWidth: 1,
-  	marginLeft: 5,
-  	marginTop: 3
+    flex: 1,
+    borderBottomColor: '#DDDDDD',
+    borderBottomWidth: 1,
+    marginLeft: 5,
+    marginTop: 3
   },
   zipCode: {
-  	width: 50,
-  	height: baseFontSize
+    width: 50,
+    height: baseFontSize,
   },
   mainText: {
-  	flex: 1,
+    flex: 1,
     fontSize: baseFontSize,
     color: '#FFFFFF'
   }
